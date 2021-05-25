ba31ac486c592b22b528ff59e479df1a7b912300
thormiwa <thormiwa04@gmail.com>
2021-05-25 14:11:24 +0100

    made changes to urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('account/', include('django.contrib.auth.urls')),
+    path('blogin/', include('blogin.urls')),
+    path('blogin/', include('django.contrib.auth.urls')),
 ]
